65734d5684b1c637044eae9c7cd0facf43dedc6a
MichaelHeinecke <47566635+MichaelHeinecke@users.noreply.github.com>
2023-07-17 18:12:49 +0200

    Removed ununsed import

--- heatwave-core/src/main/java/org/michaelheinecke/heatwave/HeatWaveApp.java
+++ heatwave-core/src/main/java/org/michaelheinecke/heatwave/HeatWaveApp.java
@@ -14,7 +14,6 @@ import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
-import org.apache.commons.logging.Log;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
