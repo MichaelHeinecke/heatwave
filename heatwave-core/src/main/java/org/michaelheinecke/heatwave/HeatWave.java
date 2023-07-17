d03a21ffd25fa4b1bf053dd89509db9fcc585dbf
MichaelHeinecke <47566635+MichaelHeinecke@users.noreply.github.com>
2023-07-17 17:42:44 +0200

    Move HeatWave and algorithm to separate class

---
+++ heatwave-core/src/main/java/org/michaelheinecke/heatwave/HeatWave.java
@@ -0,0 +1,89 @@
+package org.michaelheinecke.heatwave;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * HeatWave is home to the core algorithm of heatwave.
+ *
+ * <p>The algorithm calculates heat waves in the Netherlands following the definition of the KNMI:
+ *
+ * <p>A heat wave is a succession of at least 5 summer days (maximum temperature
+ * > 25.0 °C or higher) in De Bilt, of which at least three are tropical days
+ * > (maximum temperature 30.0 °C or higher).
+ *
+ * <p>HeatWave objects represent potential heat waves.
+ */
+class HeatWave {
+  LocalDate startDate;
+  LocalDate endDate;
+  int numberOfDays;
+  int numberOfTropicalDays;
+  double maxTemp;
+
+  HeatWave() {
+    this.startDate = null;
+    this.endDate = null;
+    this.numberOfDays = 0;
+    this.numberOfTropicalDays = 0;
+    this.maxTemp = 0.0;
+  }
+
+  boolean isHeatwave() {
+    return this.numberOfDays >= 5 && this.numberOfTropicalDays >= 3;
+  }
+
+  @Override
+  public String toString() {
+    return String.format(
+        "From date: %s, to date: %s, duration: %d, Number Tropical Days: %d, Max Temperature: %"
+            + ".1f", startDate, endDate, numberOfDays, numberOfTropicalDays, maxTemp);
+  }
+
+  /**
+   * Core algorithm to calculate heat waves.
+   *
+   * @param days A list of DailyTemperatureReading objects, sorted by date in ascending order,
+   *             and filtered for days with a maximum temperature of at least 25.0 °C.
+   * @return A list of HeatWave objects, representing a heat wave.
+   */
+  static List<HeatWave> calculateHeatWaves(List<DailyTemperatureReading> days) {
+    List<HeatWave> heatwaves = new ArrayList<>();
+    HeatWave wave = new HeatWave();
+
+    for (int i = 0; i < days.size(); i++) {
+      DailyTemperatureReading day = days.get(i);
+      if (day.maxTemperature() >= 25.0) {
+        wave.numberOfDays++;
+        if (wave.startDate == null) {
+          wave.startDate = day.date();
+        }
+      }
+
+      if (day.maxTemperature() >= 30.0) {
+        wave.numberOfTropicalDays++;
+      }
+
+      if (day.maxTemperature() > wave.maxTemp) {
+        wave.maxTemp = day.maxTemperature();
+      }
+
+      // If we reach the end of the input array,
+      // or the end of an uninterrupted sequence of days with maximum temperatures of at least 25,
+      // check if we found a heat wave.
+      if (i == days.size() - 1 || ChronoUnit.DAYS.between(day.date(), days.get(i + 1).date()) > 1) {
+        if (wave.isHeatwave()) {
+          // Calculate the inclusive end date of the wave.
+          wave.endDate = wave.startDate.plusDays(wave.numberOfDays - 1);
+          heatwaves.add(wave);
+        }
+        // Reset fields to check next potential heatwave.
+        wave = new HeatWave();
+      }
+    }
+
+    return heatwaves;
+  }
+}
--- heatwave-core/src/main/java/org/michaelheinecke/heatwave/HeatWaveApp.java
+++ heatwave-core/src/main/java/org/michaelheinecke/heatwave/HeatWaveApp.java
@@ -1,10 +1,10 @@
 package org.michaelheinecke.heatwave;
 
 
+import static org.michaelheinecke.heatwave.HeatWave.calculateHeatWaves;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -14,64 +14,15 @@ import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
 /**
- * HeatWaveApp is home to the core algorithm of heatwave.
+ * HeatWaveApp is the entry point for running heatwave.
  *
- * <p>The algorithm calculates heat waves in the Netherlands following the definition of the KNMI:
+ * <p>HeatWaveApp reads and pre-processes the data, and writes the results to a file.
  *
- * <p>A heat wave is a succession of at least 5 summer days (maximum temperature
- * > 25.0 °C or higher) in De Bilt, of which at least three are tropical days
- * > (maximum temperature 30.0 °C or higher).
- *
- * <p>The main method of this class is the entry point for running heatwave.
+ * <p>When running the app, a path to the input data files needs to be passed in.
  */
 public class HeatWaveApp {
   private static final DateTimeFormatter formatter =
       DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
-  /**
-   * Core algorithm to calculate heat waves.
-   *
-   * @param days A list of DailyTemperatureReading objects, sorted by date in ascending order,
-   *             and filtered for days with a maximum temperature of at least 25.0 °C.
-   * @return A list of HeatWave objects, representing a heat wave.
-   */
-  static List<HeatWave> calculateHeatWaves(List<DailyTemperatureReading> days) {
-    List<HeatWave> heatwaves = new ArrayList<>();
-    HeatWave wave = new HeatWave();
-
-    for (int i = 0; i < days.size(); i++) {
-      DailyTemperatureReading day = days.get(i);
-      if (day.maxTemperature() >= 25.0) {
-        wave.numberOfDays++;
-        if (wave.startDate == null) {
-          wave.startDate = day.date();
-        }
-      }
-
-      if (day.maxTemperature() >= 30.0) {
-        wave.numberOfTropicalDays++;
-      }
-
-      if (day.maxTemperature() > wave.maxTemp) {
-        wave.maxTemp = day.maxTemperature();
-      }
-
-      // If we reach the end of the input array,
-      // or the end of an uninterrupted sequence of days with maximum temperatures of at least 25,
-      // check if we found a heat wave.
-      if (i == days.size() - 1 || ChronoUnit.DAYS.between(day.date(), days.get(i + 1).date()) > 1) {
-        if (wave.isHeatwave()) {
-          // Calculate the inclusive end date of the wave.
-          wave.endDate = wave.startDate.plusDays(wave.numberOfDays - 1);
-          heatwaves.add(wave);
-        }
-        // Reset fields to check next potential heatwave.
-        wave = new HeatWave();
-      }
-    }
-
-    return heatwaves;
-  }
 
   static LocalDate parseDateTime(String string) {
     return LocalDate.parse(string, formatter);
@@ -81,11 +32,13 @@ public class HeatWaveApp {
     JavaRDD<DailyTemperatureReading> preprocessedRdd = sc.textFile(path, 8)
         // Remove header rows
         .filter(line -> !line.startsWith("#"))
+        // Parse the relevant data from the input file lines.
         .map(row ->
             new DailyTemperatureReading(LocalDate.parse(row.substring(0, 21).strip(), formatter),
                 row.substring(21, 41).strip(),
                 row.substring(309, 329).strip().equals("") ? null :
-                    Double.parseDouble(row.substring(309, 329).strip())))
+                    Double.parseDouble(row.substring(309, 329).strip()))
+        )
         // Only keeps rows for weather station De Bilt.
         .filter(row -> Objects.equals(row.location(), "260_T_a"))
         // Remove rows without temperature reading and are not potentially part of a heat wave.
@@ -119,33 +72,6 @@ public class HeatWaveApp {
       run(sc, path);
     }
 
-  }
-
-  static class HeatWave {
-    LocalDate startDate;
-    LocalDate endDate;
-    int numberOfDays;
-    int numberOfTropicalDays;
-    double maxTemp;
-
-    HeatWave() {
-      this.startDate = null;
-      this.endDate = null;
-      this.numberOfDays = 0;
-      this.numberOfTropicalDays = 0;
-      this.maxTemp = 0.0;
-    }
-
-    boolean isHeatwave() {
-      return this.numberOfDays >= 5 && this.numberOfTropicalDays >= 3;
-    }
-
-    @Override
-    public String toString() {
-      return String.format(
-          "From date: %s, to date: %s, duration: %d, Number Tropical Days: %d, Max Temperature: %"
-              + ".1f", startDate, endDate, numberOfDays, numberOfTropicalDays, maxTemp);
-    }
   }
 
 }
